52f33c01e3961059d592a1418b04468f4cf69366
Sören Grzanna <soren.grzanna@hexad.de>
2019-04-25 16:16:24 +0200

    orbitcontrols bind to canvas wrapper

--- src/component/ThreeScene.js
+++ src/component/ThreeScene.js
@@ -19,9 +19,7 @@ class ThreeScene extends Component {
     this.scene = new THREE.Scene();
     //ADD CAMERA
     this.camera = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
-    this.controls = new OrbitControls(this.camera);
-    this.camera.position.z = 100;
-    this.controls.update();
+    this.controls = new OrbitControls(this.camera, this.mount);
     //ADD RENDERER
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
@@ -38,13 +36,16 @@ class ThreeScene extends Component {
     const geometry = new THREE.SphereGeometry(5, 16, 16, 0, 6.3, 0, 3.1);
     const materialOff = new THREE.MeshLambertMaterial({
       transparent: true,
-      opacity: 0.5,
+      opacity: 0.05,
       color: 0xffd300,
       wireframe: true
     });
     const materialOn = new THREE.MeshLambertMaterial({ color: 0xffd300 });
-
+    let maxX = 0;
+    let maxY = 0;
+    let maxZ = 0;
     this.props.cube.array().forEach((led, index) => {
+      index == 100 ? led.setOn() : led.setOff();
       let material = led.isOn() ? materialOn : materialOff;
       let mesh = new THREE.Mesh(geometry, material);
       mesh.position.set(
@@ -53,7 +54,17 @@ class ThreeScene extends Component {
         led.vector().z * 15
       );
       this.scene.add(mesh);
+      maxX = Math.max(maxX, mesh.position.x);
+      maxY = Math.max(maxY, mesh.position.y);
+      maxZ = Math.max(maxZ, mesh.position.z);
     });
+    this.camera.position.z = maxZ * 2;
+    this.camera.position.x = maxX / 2;
+    this.camera.position.y = maxY / 2;
+
+    this.controls.update();
+    console.log(maxX, maxY, maxZ);
+
     this.start();
   }
 
