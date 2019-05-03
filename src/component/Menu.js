76d50a472ec7d01422c3f6bfce493dbd669166d5
Sören Grzanna <soren.grzanna@hexad.de>
2019-05-03 13:08:12 +0200

    added led control panel

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,31 +1,46 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
-import { ToggleButtonGroup, ToggleButton } from "react-bootstrap";
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  ButtonToolbar
+} from "react-bootstrap";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(value) {
+  handleChange = value => {
     this.props.setAxis(value);
-  }
+  };
 
   render() {
     return (
-      <ToggleButtonGroup
-        type="radio"
-        name="axis"
-        value={this.props.axis}
-        className="w-100"
-        size="sm"
-        onChange={this.props.setAxis}
-      >
-        <ToggleButton value="x">X</ToggleButton>
-        <ToggleButton value="y">Y</ToggleButton>
-        <ToggleButton value="z">Z</ToggleButton>
-      </ToggleButtonGroup>
+      <>
+        <ToggleButtonGroup
+          type="radio"
+          name="axis"
+          value={this.props.axis}
+          className="w-100"
+          size="sm"
+          onChange={this.props.setAxis}
+        >
+          <ToggleButton value="x">X</ToggleButton>
+          <ToggleButton value="y">Y</ToggleButton>
+          <ToggleButton value="z">Z</ToggleButton>
+        </ToggleButtonGroup>
+
+        <ButtonToolbar>
+          {this.props.cube.arrays().map(arr => (
+            <ToggleButtonGroup type="checkbox" size="sm" className="w-100">
+              {arr.map((led, k) => (
+                <ToggleButton value={k}>{k + 1}</ToggleButton>
+              ))}
+            </ToggleButtonGroup>
+          ))}
+        </ButtonToolbar>
+      </>
     );
   }
 }
